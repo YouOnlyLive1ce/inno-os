bc06769e4241b550ee0122dedc5c10ab2b794097
YouOnlyLive1ce <dimanekrasov340@gmail.com>
2023-09-12 23:53:23 +0300

    ex2 fix

--- week01/ex2.sh
+++ week01/ex2.sh
@@ -1,1 +1,25 @@
-./ex2.txt
+git reset HEAD~1
+git reset HEAD
+git clean
+cd week01
+ls
+cd ..
+ls
+clear
+git init
+git status
+git add ./week01
+git status
+git commit -m "hw1"
+git remote add origin https://github.com/YouOnlyLive1ce/inno-os.git
+git push -u origin master
+ls
+cd week01
+ls
+cat ex2.txt
+cat ex2.sh
+cut --help
+cat ex2.txt | cut -c 8- > ex2.sh
+cat ex2.sh
+cat ex2.txt
+history | tail -25 > ~/week01/ex2.txt
