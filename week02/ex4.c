c0709118b726333d150ff1d8579591f6d3d7cb01
YouOnlyLive1ce <dimanekrasov340@gmail.com>
2023-09-20 00:08:17 +0300

    ex4 fix

--- week02/ex4.c
+++ week02/ex4.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <ctype.h>
 #define MAX_LIMIT 256
 
 int count(char char_to_find, char str[MAX_LIMIT], int length){
     int counter=0;
     for (int i=0;i<length;i++){
-        if (str[i]==char_to_find)
+        if (str[i]==tolower(char_to_find) || str[i]==toupper(char_to_find))
             counter++;
     }
     return counter;
 }
 void countAll(char str[MAX_LIMIT], int length){
-    for (int i=0;i<length;i++){
+    for (int i=0;i<length-1;i++){
         if (str[i]==',')
             break;
         else 
-            printf("%c : %d,",str[i], count(str[i],str,length));
+            printf("%c : %d, ",str[i], count(str[i],str,length));
     }
 }
 
 int main(void){
     char str[MAX_LIMIT];
-	fgets(str, MAX_LIMIT, stdin);
+  fgets(str, MAX_LIMIT, stdin);
 
     int length=0;
     bool comma=false;
