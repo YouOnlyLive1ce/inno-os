d9be431b0a5b9d6cae1ce8434c8bafe6ff41fcfa
Nekrasov Dima <123145672+YouOnlyLive1ce@users.noreply.github.com>
2023-11-28 23:46:23 +0300

    fix

--- week12/ex1.c
+++ week12/ex1.c
@@ -7,9 +7,11 @@
 
 int fd;
 bool pressed_e = false;
+bool pressed_x = false;
 bool pressed_p = false;
 bool pressed_c = false;
-bool pressed_ca = false;
+bool pressed_a = false;
+char globalArray['z' - 'a' + 1];
 //custom shortcut - cp. No need in additional flags
 const char *KEYBOARD_PATH = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
 
@@ -24,8 +26,12 @@ void printEvent(struct input_event *input_event) {
                 pressed_p = false;
             if (input_event->code == KEY_C)
                 pressed_c = false;
-            if (input_event->code == KEY_A && pressed_c)
-                pressed_ca = false;
+            if (input_event->code == KEY_X)
+                pressed_x = false;
+            if (input_event->code == KEY_A)
+                pressed_a = false;
+            if (input_event->code == KEY_P)
+                pressed_p = false;
         } else if (input_event->value == 1) {
             eventType = "PRESSED";
             if (input_event->code == KEY_E)
@@ -34,8 +40,12 @@ void printEvent(struct input_event *input_event) {
                 pressed_p = true;
             if (input_event->code == KEY_C)
                 pressed_c = true;
-            if (input_event->code == KEY_A && pressed_c)
-                pressed_ca = true;
+            if (input_event->code == KEY_X)
+                pressed_x = true;
+            if (input_event->code == KEY_A)
+                pressed_a = true;
+            if (input_event->code == KEY_P)
+                pressed_p = true;
         } else if (input_event->value == 2) {
             eventType = "REPEATED";
             if (input_event->code == KEY_E)
@@ -44,32 +54,39 @@ void printEvent(struct input_event *input_event) {
                 pressed_p = true;
             if (input_event->code == KEY_C)
                 pressed_c = true;
-            if (input_event->code == KEY_A && pressed_c)
-                pressed_ca = true;
+            if (input_event->code == KEY_X)
+                pressed_x = true;
+            if (input_event->code == KEY_A)
+                pressed_a = true;
+            if (input_event->code == KEY_P)
+                pressed_p = true;
         } else
             return;
 
         FILE *outputFile = fopen("ex1.txt", "a");
         fprintf(outputFile, "%s 0x%04X (%d)\n", eventType, input_event->code, input_event->code);
         // ex bind
-        if (pressed_e == true && input_event->code == KEY_X)
-            exit(EXIT_FAILURE);
+        if (pressed_e && pressed_x)
+            exit(EXIT_SUCCESS);
         // pe bind
-        if (pressed_p == true && input_event->code == KEY_E) {
+        if (pressed_p && pressed_e) {
             fprintf(outputFile, "I passed the Exam!\n");
             pressed_p = false;
+            pressed_e=false;
         }
         // cap bind
-        if (pressed_c && pressed_ca && input_event->code == KEY_P) {
+        if (pressed_c && pressed_a && pressed_p) {
             fprintf(outputFile, "Get some cappuccino!\n");
             pressed_c = false;
-            pressed_ca = false;
+            pressed_a = false;
+            pressed_p=false;
         }
         // Funny thing. You may press c,p and not get result: order of whole sequence is important
         // When you press any other letters between pressed letters, it affect flags
-        if (pressed_c && input_event->code==KEY_P){
+        if (pressed_c && pressed_p){
             fprintf(outputFile, "You shouldn't have done that. The consequences of your action may be terrible for me\n");
             pressed_c = false;
+            pressed_p=false;
         }
         fclose(outputFile);
     }
